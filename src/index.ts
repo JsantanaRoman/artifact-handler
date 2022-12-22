e0a4e0e7dcbf4b836e4b2c2dab00192d5146ca5d
Jesus J. Santana <jsantana@navemoney.com>
2022-12-22 10:21:16 -0400

    release test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import { Probot } from "probot";
+import { Probot, ProbotOctokit } from "probot";
 
 module.exports = (app: Probot) => {
-  app.log.info("Yay, my app is loaded");
+  const octokit = new ProbotOctokit();
+  const owner = "JsantanaRoman";
+  const repo = "artifact-tester";
+
   app.on("push", async (context) => {
     let branchName = context.payload.ref;
     branchName = branchName.replace("refs/heads/", "");
-    app.log.info(branchName);
-    console.log(branchName);
-    app.log.info("hello 2");
+    if (branchName.includes("release")) {
+      const artifactList = await octokit.request(
+        "GET /repos/{owner}/{repo}/actions/artifacts",
+        {
+          owner: owner,
+          repo: repo,
+        }
+      );
+
+      const latestArtifactId = artifactList.data.artifacts[0].id;
+      const latestArtifact = await octokit.request(
+        "GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}",
+        {
+          owner: owner,
+          repo: repo,
+          artifact_id: latestArtifactId,
+        }
+      );
+      const latestArtifactUrl = latestArtifact.data.url;
+      app.log.info(latestArtifactUrl);
+    }
   });
 };
